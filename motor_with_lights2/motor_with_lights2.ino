edd1215b66300971d6886fe97e053e8764c6a33c
Zama202BC <44172640+Zama202BC@users.noreply.github.com>
2018-11-01 12:02:20 -0400

    Add files via upload

---
+++ motor_with_lights2/motor_with_lights2.ino
@@ -0,0 +1,81 @@
+
+#include <Servo.h>
+
+Servo myServo;
+
+int buttPin = 2;
+int buttSt = 0;
+int const potPin = A0;
+int potVal;
+int angle;
+int redl = 13;
+int bluel = 12;
+int greenl = 11;
+
+
+void setup() {
+  myServo.attach(9);
+  Serial.begin(9600);
+  pinMode(redl, OUTPUT);
+  pinMode(bluel, OUTPUT);
+  pinMode(greenl, OUTPUT);
+  pinMode(buttPin, INPUT);
+}
+
+void loop() {
+  printStuff();
+ 
+  myServo.write(angle);
+  buttSt =  digitalRead(buttPin);
+  if (buttSt == LOW){
+    potVal = analogRead(potPin);
+      angle = map(potVal, 0, 1023, 0, 179);
+  }
+  if (buttSt == HIGH){
+    button();
+  }
+
+
+  if (angle > 167) {
+    digitalWrite(redl, HIGH);
+  }
+
+  else if (angle < 160) {
+    digitalWrite(redl, LOW);
+  }
+
+  if (angle > 10) {
+    digitalWrite(greenl, LOW);
+  }
+
+  else if (angle < 10) {
+    digitalWrite(greenl, HIGH);
+  }
+
+  if (angle < 160 && angle > 10) {
+    digitalWrite(bluel, HIGH);
+  }
+  else if (angle > 160) {
+    digitalWrite(bluel, LOW);
+  }
+  
+ else if (angle < 10) {
+    digitalWrite(bluel, LOW);
+ }
+
+
+}
+
+void button(){
+  angle = 0;
+  
+
+}
+
+void printStuff(){
+   Serial.print("potVal: ");
+  Serial.print(potVal);
+
+  Serial.print(", angle: ");
+  Serial.println(angle);
+}
